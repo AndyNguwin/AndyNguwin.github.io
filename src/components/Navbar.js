e815e4556d78e3947a2593be337bdb660deaaebb
Andy Nguyen <iam.andynguwin@gmail.com>
2023-12-25 15:18:04 -0800

    Fixed CSS styling of buttons for navbar tabs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,14 @@
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import Button from 'react-bootstrap/Button';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-// import Row from 'react-bootstrap/Row'
-// import Col from 'react-bootstrap/Col';
 // import logo from './logo.svg';
-import './Navbar.css';
+import '../style.css';
 
 function MyNavbar() {
     return (
-        <div className='bg-navbar-bg text-navbar-text flex flex-row items-center gap-10 justify-end drop-shadow-md outline outline-navbar-outline outline-2'>
-            <div className='flex flex-row items-center gap-10'>
-                <ul className='flex flex-row items-center gap-10'>
-                    <button className="btn btn-blue"><li>About Me</li></button>
-                    <button className="btn btn-blue"><li>Skills</li></button>
-                    <button className="btn btn-blue"><li>Projects</li></button>
-                </ul>
-            </div>
+        <div className='bg-navbar-bg text-navbar-text drop-shadow-md outline outline-navbar-outline outline-2 items-center'>
+            <ul className='flex flex-row items-center justify-end gap-4 px-4'>
+                <button className="btn-blue"><li>About Me</li></button>
+                <button className="btn-blue"><li>Skills</li></button>
+                <button className="btn-blue"><li>Projects</li></button>
+            </ul>
         </div>
     );
 }
