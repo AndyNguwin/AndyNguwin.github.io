bd0792f05eb5d9f2a777bd5b956a3dc894f441ef
Andy Nguyen <iam.andynguwin@gmail.com>
2025-04-19 14:15:25 -0700

    Added Tabs for portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import { Button } from '@mui/material'
-import { Grid, Card, Paper, Typography } from '@mui/material'
+import { Grid, Tabs, Tab, Paper, Typography } from '@mui/material'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tab, setTab] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newTab: number) => {
+    setTab(newTab);
+  }
 
   return (
     <>
@@ -14,11 +18,20 @@ function App() {
         <Grid container spacing={2} sx={{minHeight: '100vh', width: '100vw', maxWidth: '100%'}}>
           <Grid size={3} sx={{height: '50vh', alignContent: 'center', position: 'sticky', top: 0}}>
             <Paper elevation={3} sx={{backgroundColor: '#091425', height: '100%'}}>
-              <Typography color='common.white'> Yes </Typography>
+            <Typography color='common.white'> Andy Nguyen </Typography>
+              <Typography color='common.white'> Email </Typography>
+              <Typography color='common.white'> LinkedIn </Typography>
+              <Typography color='common.white'> Github </Typography>
+              <Typography color='common.white'> Phone </Typography>
             </Paper>
           </Grid>
           <Grid size={9} sx={{height: '50vh', alignContent: 'center'}}>
             <Paper elevation={3} sx={{backgroundColor: '#091425', height: '100%'}}>
+              <Tabs value={tab} onChange={handleChange}>
+                <Tab label="About Me"/>
+                <Tab label="Resume"/>
+              </Tabs>
+
               <Typography color='common.white'> Yes </Typography>
             </Paper>
           </Grid>
